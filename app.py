4ef0fcf87331dd12fca2d997eb49808e60f60dfc
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-04-23 16:22:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('plant_classifier.hdf5')
+  model=tf.keras.models.load_model('MultiWeatherCNN.hdf5')
   return model
 model=load_model()
 st.write("""
-# Plant Leaf Detection System"""
+# Weather System"""
 )
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
+file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
 
 import cv2
 from PIL import Image,ImageOps
@@ -27,22 +27,9 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['Alpinia Galanga (Rasna)','Amaranthus Viridis (Arive-Dantu)',
-                 'Artocarpus Heterophyllus (Jackfruit)',
-                 'Azadirachta Indica (Neem)','Basella Alba (Basale)',
-                 'Brassica Juncea (Indian Mustard)','Carissa Carandas (Karanda)',
-                 'Citrus Limon (Lemon)','Ficus Auriculata (Roxburgh fig)',
-                 'Ficus Religiosa (Peepal Tree)','Hibiscus Rosa-sinensis',
-                 'Jasminum (Jasmine)','Mangifera Indica (Mango)',
-                 'Mentha (Mint)','Moringa Oleifera (Drumstick)',
-                 'Muntingia Calabura (Jamaica Cherry-Gasagase)',
-                 'Murraya Koenigii (Curry)', 'Nerium Oleander (Oleander)',
-                 'Nyctanthes Arbor-tristis (Parijata)','Ocimum Tenuiflorum (Tulsi)',
-                 'Piper Betle (Betel)','Plectranthus Amboinicus (Mexican Mint)',
-                 'Pongamia Pinnata (Indian Beech)','Psidium Guajava (Guava)',
-                 'Punica Granatum (Pomegranate)','Santalum Album (Sandalwood)',
-                 'Syzygium Cumini (Jamun)','Syzygium Jambos (Rose Apple)',
-                 'Tabernaemontana Divaricata (Crape Jasmine)',
-                 'Trigonella Foenum-graecum (Fenugreek)']
+    class_names=['Sunrise',
+                 'Shine',
+                 'Rain',
+                 'Cloudy']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
